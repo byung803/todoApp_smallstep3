45f3fb188ab8b79c3bededa17ef87151ae9806e5
BYUNGMIN CHOI <byung803@gmail.com>
2018-10-20 12:12:51 +0900

    add action generator test

--- src/tests/actions/index.test.js
+++ src/tests/actions/index.test.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Index from '../../actions/index';
+import { addTodo, removeTodo, setVisibilityFilter, toggleTodo } from '../../actions/index';
 
-test('should test actions index', () => {
-
+test('should test addTodo action generator', () => {
+    const text = 'test text';
+    expect(addTodo(text)).toEqual({
+        type: 'ADD_TODO',
+        id: expect.any(Number),
+        text
+    })
 });
+
+test('should test removeTodo action generator', () => {
+    const id = 123;
+    expect(removeTodo(id)).toEqual({
+        type: "REMOVE_TODO",
+        id
+    })
+});
+
+test('should test setVisibilityFilter action generator', () => {
+    const filter = 'FILTER';
+    expect(setVisibilityFilter(filter)).toEqual({
+        type: 'SET_VISIBILITY_FILTER',
+        filter
+    })
+});
+
+test('should test toggleTodo action generator', () => {
+    const id = 1234;
+    expect(toggleTodo(id)).toEqual({
+        type: 'TOGGLE_TODO',
+        id
+    })
+})
